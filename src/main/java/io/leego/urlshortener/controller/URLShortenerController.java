24c003a4a08c90d567e8b85041e0821c6a771c07
leego <leegodev@hotmail.com>
2022-09-08 10:46:14 +0800

    Remove redis

--- src/main/java/io/leego/urlshortener/controller/URLShortenerController.java
+++ src/main/java/io/leego/urlshortener/controller/URLShortenerController.java
@@ -1,11 +1,10 @@
 package io.leego.urlshortener.controller;
 
+import com.google.common.cache.CacheBuilder;
 import com.google.common.hash.HashCode;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Base64;
-import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentMap;
 import java.util.stream.IntStream;
 
 /**
@@ -24,28 +25,22 @@ import java.util.stream.IntStream;
  */
 @RestController
 public class URLShortenerController {
-    private final StringRedisTemplate redisTemplate;
-    private final DefaultRedisScript<Boolean> script;
     private final HashFunction[] hashes;
+    private final ConcurrentMap<Object, Object> cache;
 
-    public URLShortenerController(StringRedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-        this.script = new DefaultRedisScript<>("""
-                local o = redis.call('GET', KEYS[1])
-                if not o then
-                    redis.call('SET', KEYS[1], ARGV[1])
-                    redis.call('EXPIRE', KEYS[1], ARGV[2])
-                    return true
-                end
-                if o == ARGV[1] then
-                    return true
-                end
-                return false""", Boolean.class);
+    public URLShortenerController() {
         // For hash collisions
         this.hashes = IntStream.iterate(0, n -> n + 1)
                 .mapToObj(Hashing::murmur3_32_fixed)
                 .limit(16)
                 .toArray(HashFunction[]::new);
+        this.cache = CacheBuilder.newBuilder()
+                .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                .initialCapacity(1 << 10)
+                .maximumSize(1 << 14)
+                .expireAfterAccess(Duration.ofDays(1))
+                .build()
+                .asMap();
     }
 
     @PostMapping("/")
@@ -54,8 +49,8 @@ public class URLShortenerController {
             HashCode hashCode = hash.hashString(url, StandardCharsets.UTF_8);
             // TODO Base62 is better
             String s = Base64.getUrlEncoder().withoutPadding().encodeToString(hashCode.asBytes());
-            Boolean b = redisTemplate.execute(script, List.of(s), url, /* timeout */ "86400");
-            if (b != null && b) {
+            Object old = cache.putIfAbsent(s, url);
+            if (old == null || Objects.equals(url, old)) {
                 return s;
             }
         }
@@ -65,9 +60,9 @@ public class URLShortenerController {
 
     @GetMapping("/{hash}")
     public RedirectView redirect(@PathVariable String hash, HttpServletResponse response) {
-        String url = redisTemplate.opsForValue().get(hash);
+        Object url = cache.get(hash);
         if (url != null) {
-            RedirectView v = new RedirectView(url);
+            RedirectView v = new RedirectView((String) url);
             // 301 is better than 302
             v.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
             return v;
